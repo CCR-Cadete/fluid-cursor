9185ff043bccfb1eae1e9e0f89b35c233fa1e34d
Caio Cadete Ramos <caiocadete20@gmail.com>
2026-04-11 04:51:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ let config = {
     COLOR_UPDATE_SPEED: 10,
     PAUSED: false,
     BACK_COLOR: { r: 0, g: 0, b: 0 },
-    TRANSPARENT: true,
+    TRANSPARENT: false,
     BLOOM: false,
     BLOOM_ITERATIONS: 8,
     BLOOM_RESOLUTION: 256,
